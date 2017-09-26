41525168edbaba10100724b91bb9d2f5488d5fb6
Ja <prince36@onet.pl>
2017-09-26 19:56:06 +0200

    Initial commit

---
+++ src/main/java/com/portal/evegsa/EvegsaApplication.java
@@ -0,0 +1,12 @@
+package com.portal.evegsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvegsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvegsaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/portal/evegsa/EvegsaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.portal.evegsa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EvegsaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
